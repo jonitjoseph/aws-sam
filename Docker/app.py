a233743498d952fb0e714abac76bbf1f39bf4d5c
jonitjoseph <jonitjoseph@gmail.com>
2022-01-05 20:53:50 +0530

    stable version

--- Docker/app.py
+++ Docker/app.py
@@ -1,12 +1,28 @@
 import json
 from selenium import webdriver
+from tempfile import mkdtemp
 
 def lambda_handler(event, context):
 
-    driver = webdriver.Chrome(executable_path='/opt/chromedriver/96.0.4664.45/chromedriver_linux64')
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/opt/chrome/chrome'
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument(f"--user-data-dir={mkdtemp()}")
+    options.add_argument(f"--data-path={mkdtemp()}")
+    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    options.add_argument("--remote-debugging-port=9222")
 
-    driver.get("https://www.google.co.uk")
-    print(driver.title)
+    chrome = webdriver.Chrome(executable_path="/opt/chromedriver", options=options)
+
+    chrome.get("https://www.example.com")
+    print(chrome.title)
 
     return {
     "statusCode": 200,
@@ -14,6 +30,6 @@ def lambda_handler(event, context):
             "Content-Type": "application/json"
         },
         "body": json.dumps({
-            "title ": driver.title
+            "title ": chrome.title
         })
     }
